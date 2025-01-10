3e2e51082b2e78b5228cd8a1e8903ede099f4275
Andrew <unseenmocha@gmail.com>
2025-01-10 01:31:35 -0500

    added top menu bar

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        fontFamily: {
+          "funnel-display": ["Funnel Display",] //  "Light 300", "Regular 400", "Medium 500", "SemiBold 600", "Bold 700"
+        }
+      },
+    },
+    plugins: [],
+
+  }
